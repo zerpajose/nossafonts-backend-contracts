69883a315d161b626b13e89315999b71ec028b96
CryptoVincent <zjose88@gmail.com>
2022-11-23 03:07:05 -0300

    fix upload font module

--- app.js
+++ app.js
@@ -1,49 +1,54 @@
-import express from 'express'
-import { Blob } from 'buffer'
-import cors from 'cors'
-import multer from 'multer'
-import fs from 'fs'
+import express from "express";
+import { Blob } from "buffer";
+import cors from "cors";
+import multer from "multer";
+import fs from "fs";
 
-import { getIsAllowed, storeFileToIPFS, storeCSSToIPFS, saveToTable, selectAll, selectSearch, storeMetadataToIPFS } from './lib/functions.js'
+import {
+  getIsAllowed,
+  storeFileToIPFS,
+  storeCSSToIPFS,
+  saveToTable,
+  selectAll,
+  selectSearch,
+  storeMetadataToIPFS,
+} from "./lib/functions.js";
 
-const PORT = 3000
+const PORT = 3000;
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
-const app = express()
-
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`)
-})
+  console.log(`Server started at port ${PORT}`);
+});
 
 app.post("/login", async (req, res) => {
-  const token = req.headers['authorization']
+  const token = req.headers["authorization"];
 
-  const { address } = await Web3Token.verify(token)
+  const { address } = await Web3Token.verify(token);
 
-  res.cookie('token', token)
+  res.cookie("token", token);
 
-  res.json({address: address, token: token})
-  
-  res.end()
-})
+  res.json({ address: address, token: token });
 
-app.get("/fonts", (req, res) => {
+  res.end();
+});
 
-})
+app.get("/fonts", (req, res) => {});
 
 app.get("/fonts/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
-  res.json({respuesta: id})
-  res.end()
-})
+  res.json({ respuesta: id });
+  res.end();
+});
 
-app.post("/fonts", upload.single('file'), async (req, res) => {
-
+app.post("/fonts", upload.single("file"), async (req, res) => {
   /* web3 token authorization */
   /*
   const token = req.cookies.token
@@ -58,74 +63,62 @@ app.post("/fonts", upload.single('file'), async (req, res) => {
   }
   */
 
-  const { name } = req.body
-  //console.log(`req.file: ${JSON.stringify(req.file)}`)
-  
-  const { filename, originalname, buffer } = req.file
+  const { name } = req.body;
 
-  const file_blob = new Blob([buffer])
-  // const file = new File([buffer], originalname)
+  const { filename, originalname, buffer } = req.file;
 
-  // const cid_file = await storeFileToIPFS(file)
-  const cid_font_file = await storeFileToIPFS(file_blob)
-  const cid_css_file = await storeCSSToIPFS(name, originalname, cid_font_file)
-  const cid_metadata_file = await storeMetadataToIPFS(name, cid_css_file)
+  const file_blob = new Blob([buffer]);
+
+  const cid_font_file = await storeFileToIPFS(file_blob);
+  const cid_css_file = await storeCSSToIPFS(name, originalname, cid_font_file);
+  const cid_metadata_file = await storeMetadataToIPFS(name, cid_css_file);
 
   fs.unlinkSync(`./uploads/${filename}`);
 
   res.json({
     name: name,
     cid_css_file: cid_css_file,
-    cid_metadata_file: cid_metadata_file
-  })
-  
-  res.end()
-})
+    cid_metadata_file: cid_metadata_file,
+  });
+
+  res.end();
+});
 
 app.get("/sql", async (req, res) => {
+  const results = await selectAll();
 
-  const results = await selectAll(req.params.s)
+  if (!results.length > 0) {
+    res.json([]);
+  } else {
+    res.json(results);
+  }
 
-  if(!results.length > 0){
-    res.json([])
-  }
-  else {
-    res.json(
-      results
-    )
-  }
-  
-  res.end()
-})
+  res.end();
+});
 
 app.get("/sql/:s", async (req, res) => {
+  const search = req.params.s;
 
-  const search = req.params.s
-  
-  if(search.length <= 2){
-    res.send("Search by 3 or more characters")
-  }
-  else{
-    const {columns, rows} = await selectSearch(search)
-    
-    if(!rows.length > 0){
-      res.json([])
-    }
-    else {
-      res.json(
-        rows
-      )
+  if (search.length <= 2) {
+    res.send("Search by 3 or more characters");
+  } else {
+    const { columns, rows } = await selectSearch(search);
+
+    if (!rows.length > 0) {
+      res.json([]);
+    } else {
+      res.json(rows);
     }
   }
-  
-  res.end()
-})
+
+  res.end();
+});
 
 app.post("/sql", async (req, res) => {
-  const { id, name } = req.body
+  const { id, name } = req.body;
 
-  const txHash = await saveToTable(id, name)
+  const txHash = await saveToTable(id, name);
 
-  res.json({txHash: txHash})
-  res.end()
-})
+  res.json({ txHash: txHash });
+  res.end();
+});
