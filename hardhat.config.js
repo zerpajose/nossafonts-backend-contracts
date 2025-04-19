8205d96cc618605ca78b64b44bd9540a4fa1c103
Jose Zerpa <zjose88@gmail.com>
2025-04-19 01:07:34 -0400

    rearrange envs

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,14 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const DEPLOY_WALLET_PRIVATE_KEY = process.env.DEPLOY_WALLET_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     mumbai: {
       url: ALCHEMY_API_KEY_URL,
-      accounts: [PRIVATE_KEY],
+      accounts: [DEPLOY_WALLET_PRIVATE_KEY],
     },
   },
 };
